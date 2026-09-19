a8e688a906303581596f1b733e0145e56ab5ecb7
Vanilson Sousa <vanilsonped7@gmail.com>
2026-09-19 00:36:32 -0300

    feat: adding swagger

--- src/Backend/SGFBA.Api/Program.cs
+++ src/Backend/SGFBA.Api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
@@ -13,6 +14,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
