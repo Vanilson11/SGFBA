d3e0f0497c83fb4a7e7054144d9e021c8a89957f
Vanilson Sousa <vanilsonped7@gmail.com>
2026-09-18 23:49:11 -0300

    feat: adding and configuring dbContext and Dependency Injection Service on Infrastructure

---
+++ src/Backend/SGFBA.Infrastructure/DataAccess/SGFBADbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGFBA.Domain.Entities;
+
+namespace SGFBA.Infrastructure.DataAccess;
+
+internal class SGFBADbContext : DbContext
+{
+    public SGFBADbContext(DbContextOptions options): base(options) {}
+
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Estudante> Estudantes { get; set; }
+    public DbSet<Ficha> Fichas { get; set; }
+    public DbSet<Acao> Acoes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Usuario>().ToTable("usuarios");
+        modelBuilder.Entity<Estudante>().ToTable("estudantes");
+        modelBuilder.Entity<Ficha>().ToTable("fichas");
+        modelBuilder.Entity<Acao>().ToTable("acoes");
+    }
+}
---
+++ src/Backend/SGFBA.Infrastructure/DependencyInjectionExtentions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using SGFBA.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace SGFBA.Infrastructure;
+
+public static class DependencyInjectionExtentions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddDbContext(services, configuration);
+    }
+
+    private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        var connection = configuration.GetConnectionString("Connection");
+
+        services.AddDbContext<SGFBADbContext>(config =>
+        {
+            config.UseMySQL(connection!);
+        });
+    }
+}
