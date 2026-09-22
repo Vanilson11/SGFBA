6a0e7853e72b2f95cd3456e92699baf154b7a5c7
Vanilson Sousa <vanilsonsousa7@hotmail.com>
2026-09-22 13:46:17 -0300

    feat: validating request change password on change password use case

--- src/Backend/SGFBA.Application/UseCases/Ususarios/AlterarSenha/AlterarSenhaUsuarioUseCase.cs
+++ src/Backend/SGFBA.Application/UseCases/Ususarios/AlterarSenha/AlterarSenhaUsuarioUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using SGFBA.Communication.Requests;
+using SGFBA.Exception.ExceptionsBase;
 
 namespace SGFBA.Application.UseCases.Ususarios.AlterarSenha;
 
@@ -6,6 +7,18 @@ public class AlterarSenhaUsuarioUseCase : IAlterarSenhaUsuarioUseCase
 {
     public async Task Executar(RequestAlterarSenhaJson request)
     {
-        throw new NotImplementedException();
+        Validar_Request(request);
+    }
+
+    private void Validar_Request(RequestAlterarSenhaJson request)
+    {
+        var resultado = new AlterarSenhaValidator().Validate(request);
+
+        if(resultado.IsValid is false)
+        {
+            var mensagensErro = resultado.Errors.Select(erro => erro.ErrorMessage).ToList();
+
+            throw new ErrosNaValidacaoException(mensagensErro);
+        }
     }
 }
---
+++ src/Backend/SGFBA.Application/UseCases/Ususarios/AlterarSenha/AlterarSenhaValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using SGFBA.Communication.Requests;
+
+namespace SGFBA.Application.UseCases.Ususarios.AlterarSenha;
+
+public class AlterarSenhaValidator : AbstractValidator<RequestAlterarSenhaJson>
+{
+    public AlterarSenhaValidator()
+    {
+        RuleFor(request => request.NovaSenha).SetValidator(new SenhaValidator<RequestAlterarSenhaJson>());
+    }
+}
