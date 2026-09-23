5b80c8d54774ea4d47e0943416b47cce27cc71d5
Vanilson Sousa <vanilsonped7@gmail.com>
2026-09-22 22:29:19 -0300

    feat: finish cancel ficha use case

--- src/Backend/SGFBA.Application/UseCases/Fichas/Cancelar/CancelarFichaUseCase.cs
+++ src/Backend/SGFBA.Application/UseCases/Fichas/Cancelar/CancelarFichaUseCase.cs
@@ -1,9 +1,39 @@
-﻿namespace SGFBA.Application.UseCases.Fichas.Cancelar;
+﻿using SGFBA.Domain.Repositories;
+using SGFBA.Domain.Repositories.Fichas;
+using SGFBA.Domain.Services.LoggedUser;
+using SGFBA.Exception;
+using SGFBA.Exception.ExceptionsBase;
+
+namespace SGFBA.Application.UseCases.Fichas.Cancelar;
 
 public class CancelarFichaUseCase : ICancelarFichaUseCase
 {
+    private readonly IUpdateOnlyFichasRepository _updateOnlyFichasRepository;
+    private readonly ILoggedUser _loggedUser;
+    private readonly IUnitOffWork _unitOffWork;
+
+    public CancelarFichaUseCase(
+        IUpdateOnlyFichasRepository updateOnlyFichasRepository,
+        ILoggedUser loggedUser,
+        IUnitOffWork unitOffWork
+        )
+    {
+        _updateOnlyFichasRepository = updateOnlyFichasRepository;
+        _loggedUser = loggedUser;
+        _unitOffWork = unitOffWork;
+    }
     public async Task Executar(long idFicha)
     {
-        throw new NotImplementedException();
+        var usuarioLogado = await _loggedUser.Get();
+
+        var ficha = await _updateOnlyFichasRepository.BuscarPorId(usuarioLogado.Id, idFicha);
+
+        if (ficha is null) throw new NotFoundException(ResourceErrorMessages.FICHA_NAO_ENCONTRADA);
+
+        ficha.Status = Domain.Enums.StatusFicha.Cancelada;
+
+        _updateOnlyFichasRepository.Atualizar(ficha);
+
+        await _unitOffWork.Commit();
     }
 }
--- src/Backend/SGFBA.Infrastructure/DataAccess/SGFBADbContext.cs
+++ src/Backend/SGFBA.Infrastructure/DataAccess/SGFBADbContext.cs
@@ -35,6 +35,7 @@ internal class SGFBADbContext : DbContext
             .WithMany()
             .HasForeignKey(e => e.IdEstudante);
         modelBuilder.Entity<Ficha>().ToTable("fichas");
+        //modelBuilder.Entity<Ficha>().HasQueryFilter(f => f.Status != Domain.Enums.StatusFicha.Cancelada);
 
 
         modelBuilder.Entity<Acao>()
