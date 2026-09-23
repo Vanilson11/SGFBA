6eb3f1ead5134f8eee596d8cf9c05ce8021d9aba
Vanilson Sousa <vanilsonped7@gmail.com>
2026-09-23 10:35:18 -0300

    feat: finishing get ficha by id use case

--- src/Backend/SGFBA.Application/UseCases/Fichas/BuscarAtivaPorId/BuscarFichaAtivaPorIdUseCase.cs
+++ src/Backend/SGFBA.Application/UseCases/Fichas/BuscarAtivaPorId/BuscarFichaAtivaPorIdUseCase.cs
@@ -1,11 +1,39 @@
 ﻿using SGFBA.Communication.Responses;
+using SGFBA.Domain.Repositories.Fichas;
+using SGFBA.Domain.Services.LoggedUser;
+using SGFBA.Exception;
+using SGFBA.Exception.ExceptionsBase;
 
 namespace SGFBA.Application.UseCases.Fichas.BuscarAtivaPorId;
 
 public class BuscarFichaAtivaPorIdUseCase : IBuscarFichaAtivaPorIdUseCase
 {
+    private readonly IReadOnlyFichasRepository _readOnlyFichasRepository;
+    private readonly ILoggedUser _loggedUser;
+
+    public BuscarFichaAtivaPorIdUseCase(
+        IReadOnlyFichasRepository readOnlyFichasRepository,
+        ILoggedUser loggedUser
+        )
+    {
+        _readOnlyFichasRepository = readOnlyFichasRepository;
+        _loggedUser = loggedUser;
+    }
     public async Task<ResponseFichaJson> Executar(long idFicha)
     {
-        throw new NotImplementedException();
+        var usuarioLogado = await _loggedUser.Get();
+
+        var ficha = await _readOnlyFichasRepository.BuscarPorId(usuarioLogado.Id, idFicha);
+
+        if (ficha is null) throw new NotFoundException(ResourceErrorMessages.FICHA_NAO_ENCONTRADA);
+
+        return new ResponseFichaJson
+        {
+            Id = ficha.Id,
+            DataAbertura = ficha.DataAbertura,
+            Motivo = (Communication.Enums.Motivo)ficha.Motivo,
+            Status = (Communication.Enums.StatusFicha)ficha.Status,
+            Observacao = ficha.Observacao
+        };
     }
 }
